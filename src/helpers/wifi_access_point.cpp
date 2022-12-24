2bbbf56e8a6e67ed96250c3e2e6ecb6140da5d7e
MDReal32 <veysaliyev00@gmail.com>
2022-12-24 09:45:45 -0500

    isHidden

--- src/helpers/wifi_access_point.cpp
+++ src/helpers/wifi_access_point.cpp
@@ -18,6 +18,14 @@ char *WifiAccessPoint::get_mac_address() {
   return WifiAccessPoint::mac_address;
 }
 
-void WifiAccessPoint::set_mac_address(char *mac) {
-  WifiAccessPoint::mac_address = mac;
+void WifiAccessPoint::set_mac_address(char *macAddress) {
+  WifiAccessPoint::mac_address = macAddress;
+}
+
+bool WifiAccessPoint::get_is_hidden() {
+  return WifiAccessPoint::is_hidden;
+}
+
+void WifiAccessPoint::set_is_hidden(bool isHidden) {
+  WifiAccessPoint::is_hidden = isHidden;
 }
