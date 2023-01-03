a112b29f14e926ffa00b37c4c9bc5dacf8b003b3
MDReal32 <veysaliyev00@gmail.com>
2023-01-03 06:31:22 -0500

    change validate form

--- cpp/core/get_ifname.cpp
+++ cpp/core/get_ifname.cpp
@@ -9,7 +9,8 @@ string get_ifname() {
   if (if_nidxs != nullptr) {
     for (intf = if_nidxs; intf->if_index != 0 || intf->if_name != nullptr; intf++) {
       string ifname = intf->if_name;
-      if (ifname.starts_with(prefix)) {
+
+      if (ifname.find(prefix) != string::npos) {
         if_freenameindex(if_nidxs);
         return ifname;
       }
