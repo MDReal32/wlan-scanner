02656f93a601f00f89e8909a39e516756b6c014e
MDReal32 <veysaliyev00@gmail.com>
2023-01-03 07:59:43 -0500

    log accessPoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { getAccessPoints } from "./utils/getAccessPoints";
 
 (async () => {
-  // infinite loop on 1 second interval
-  while (true) {
-    console.log(getAccessPoints().length);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  console.log(getAccessPoints().length);
 })();
